7ff5a2891885767f560bf75174b55c6e74fa05ab
Ingy döt Net <ingy@ingy.net>
2012-07-15 13:16:13 -0700

    Released version 0.01

---
+++ lib/Alt.pm
@@ -0,0 +1,33 @@
+package Alt;
+
+$VERSION = '0.01';
+
+__END__
+
+=head1 NAME
+
+Alt - Alternate Module Implementations
+
+=head1 SYNOPSIS
+
+    cpanm Alt::IO::All::mst
+
+=head1 DESCRIPTION
+
+C<Alt::> is the namespace for alternate implementations of CPAN modules. This
+documentation is intended to explain this concept, how it works, and
+guidelines for using it well.
+
+... documentation in progress ...
+
+=head1 AUTHOR
+
+Ingy döt Net <ingy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2012. Ingy döt Net.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
